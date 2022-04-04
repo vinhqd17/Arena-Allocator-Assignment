b5956b81c80e2ea1ced2afc5c8c8eaf353611f44
tri2401 <tri2401na@gmail.com>
2022-04-04 12:23:28 -0500

    destroy version 1

--- Office-Hour-Example-Code/mavalloc.c
+++ Office-Hour-Example-Code/mavalloc.c
@@ -1,18 +1,18 @@
 // The MIT License (MIT)
-// 
-// Copyright (c) 2022 Trevor Bakker 
-// 
+//
+// Copyright (c) 2022 Trevor Bakker
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
-// 
+//
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -49,7 +49,7 @@ enum ALGORITHM allocation_algorithm = FIRST_FIT;
 int mavalloc_init( size_t size, enum ALGORITHM algorithm )
 {
   arena = malloc( ALIGN4( size ) );
-  
+
   allocation_algorithm = algorithm;
 
   alloc_list = ( struct Node * )malloc( sizeof( struct Node ));
@@ -68,8 +68,15 @@ int mavalloc_init( size_t size, enum ALGORITHM algorithm )
 void mavalloc_destroy( )
 {
   free( arena );
-  
-  // iterate over the linked list and free the nodes
+
+  struct * Node temp;
+
+  while (alloc_list)
+  {
+      temp = alloc_list;
+      alloc_list = alloc_list->next;
+      free(temp);
+  }
 
   return;
 }
@@ -79,7 +86,7 @@ void * mavalloc_alloc( size_t size )
   struct Node * node;
 
   if( allocation_algorithm != NEXT_FIT )
-  { 
+  {
     node = alloc_list;
   }
   else if ( allocation_algorithm == NEXT_FIT )
@@ -101,21 +108,21 @@ void * mavalloc_alloc( size_t size )
       if( node -> size >= aligned_size  && node -> type == FREE )
       {
         int leftover_size = 0;
-  
+
         node -> type  = USED;
         leftover_size = node -> size - aligned_size;
         node -> size =  aligned_size;
-  
+
         if( leftover_size > 0 )
         {
           struct Node * previous_next = node -> next;
           struct Node * leftover_node = ( struct Node * ) malloc ( sizeof( struct Node ));
-  
+
           leftover_node -> arena = node -> arena + size;
           leftover_node -> type  = FREE;
           leftover_node -> size  = leftover_size;
           leftover_node -> next  = previous_next;
-  
+
           node -> next = leftover_node;
         }
         previous_node = node;
@@ -145,7 +152,7 @@ int mavalloc_size( )
   while( ptr )
   {
     number_of_nodes ++;
-    ptr = ptr -> next; 
+    ptr = ptr -> next;
   }
 
   return number_of_nodes;
