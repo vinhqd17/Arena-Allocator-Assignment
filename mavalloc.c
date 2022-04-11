4fe0115cf0d9458594b5dd7635ab1006bf5a043e
tri2401 <tri2401na@gmail.com>
2022-04-10 21:26:06 -0500

    final version?

--- mavalloc.c
+++ mavalloc.c
@@ -1,4 +1,11 @@
+//Vinh Dao 1001683799
+//Augustine Nguyen 1001654628
+//3320-003
+
 // The MIT License (MIT)
+//
+// Copyright (c) 2022 Trevor Bakker
+//
 //
 // Copyright (c) 2022 Trevor Bakker
 //
@@ -9,8 +16,11 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
+//
+//
 //
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -48,6 +58,10 @@ void * arena;
 
 enum ALGORITHM allocation_algorithm = FIRST_FIT;
 
+//Allocates arena and alloc_list using malloc
+//Initializes the List with defult/empty values to be set later in mavalloc
+    //Things like:  arena, size, type, next, previous_next
+//Also sets previous_node to alloc_list for use in next fit later
 int mavalloc_init( size_t size, enum ALGORITHM algorithm )
 {
   arena = malloc( ALIGN4( size ) );
@@ -67,7 +81,9 @@ int mavalloc_init( size_t size, enum ALGORITHM algorithm )
   return 0;
 }
 
-void mavalloc_destroy( )
+//Destroys current list and arena
+//frees arena and then traverses alloc_list to free each node individually (making sure to save the current node before freeing it, in order to traverse)
+void mavalloc_destroy()
 {
   free( arena );
 
@@ -85,8 +101,14 @@ void mavalloc_destroy( )
 
 void * mavalloc_alloc( size_t size )
 {
+  // only return NULL on failure
   struct Node * node;
 
+  //Sets node to be traversed depending on which allocating algorithm
+  //All algos excepts next fit start at the start of the List
+  //Next fit starts at the previous_node, wherever allocation left off
+    //In the case that it doesn't have a value, it just goes back to the top of the list
+  //If the algo is invalid, errors
   if( allocation_algorithm != NEXT_FIT )
   {
     node = alloc_list;
@@ -102,8 +124,17 @@ void * mavalloc_alloc( size_t size )
     exit(0);
   }
 
+  //get aligned size
   size_t aligned_size = ALIGN4( size );
 
+  //First fit
+  //Traverses from top of the list
+  //Tracks to see which node isn't used and of the correct size
+  //Once a node is found to fit both parameters, changes node attributes to the correct size and type
+  //A new node is made for any leftover space that wasn't allocated to the new nodes, splitting the previous node blocks
+  //New node is inserted, being pointed to by the allocated node and pointing to the previous next node
+  //Also sets previous node to the allocated node, allowing first fit to start from there
+  //returns the arena
   if( allocation_algorithm == FIRST_FIT )
   {
     while( node != NULL )
@@ -135,7 +166,11 @@ void * mavalloc_alloc( size_t size )
     }
   }
 
-  // Implement Next Fit
+  //Next Fit
+  //Does the EXACT same thing as first fit
+  //EXCEPT, it starts from last allocated nodes
+  //Does this by utilising the previous_node rather than the top of the list
+  //Also returns arena
   if (allocation_algorithm == NEXT_FIT)
   {
     while (node != NULL)
@@ -172,6 +207,15 @@ void * mavalloc_alloc( size_t size )
     }
   }
 
+  // Traverse through the whole Linked List
+  // to find a portion of memory(node) where it is free,
+  // the size of that free memory minus the passed in size
+  // is less than the smallest (int) in order to find
+  // the worst possible fit and the size of free
+  // memory is greater than or equal to the requested size
+  // set the winning node to be that node and size to be
+  // the size of that memory minus that passed in size
+  // Implement Worst Fit
   struct Node *winner = NULL;
   int winning_size = INT_MAX;
   int losing_size = INT_MIN;
@@ -180,7 +224,7 @@ void * mavalloc_alloc( size_t size )
   {
     while(node != NULL)
     {
-      if(node -> type == FREE && ((node -> size - size) > losing_size) && node->size >= aligned_size)
+      if(node -> type == FREE && ((node -> size - size) < losing_size) && node->size >= aligned_size)
       {
         winner = node;
         losing_size = node -> size - size;
@@ -190,6 +234,14 @@ void * mavalloc_alloc( size_t size )
     return winner;
   }
 
+  // Traverse through the whole Linked List
+  // to find a portion of memory(node) where it is free,
+  // the size of that free memory minus the passed in size
+  // is less than the largest (int) in order to find
+  // the best possible fit and the size of free
+  // memory is greater than or equal to the requested size
+  // set the winning node to be that node and size to be
+  // the size of that memory minus that passed in size
   // Implement Best Fit
   if(allocation_algorithm == BEST_FIT)
   {
@@ -202,13 +254,19 @@ void * mavalloc_alloc( size_t size )
       }
       node = node -> next;
     }
-
     return winner;
   }
 
   return NULL;
 }
 
+//Ptr is of type void, however, it's true value is arena
+//Traverse list and looks for node with matching arena ptr values
+//Once found, node is set to type free
+//Traverse list again to look for consecutive free nodes
+//If found, saves next node for freeing later, sets front node to have the size of both its current size and the next node size, and then sets the next node to the next next node, skipping over the previous next node
+//then frees the previous next node
+//essentially, combining nodes
 void mavalloc_free( void * ptr )
 {
   //courtesy of Professor Bakker
@@ -246,11 +304,10 @@ void mavalloc_free( void * ptr )
     }
     node = node -> next;
   }
-
   return;
-
 }
 
+//traverses list and counts the number of nodes in list
 int mavalloc_size( )
 {
   int number_of_nodes = 0;
@@ -261,6 +318,5 @@ int mavalloc_size( )
     number_of_nodes ++;
     ptr = ptr -> next;
   }
-
   return number_of_nodes;
 }
